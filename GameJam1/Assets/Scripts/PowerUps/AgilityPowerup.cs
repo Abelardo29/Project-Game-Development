415c3c76853bc7cd354e0b1489c790ac3df7b357
JANPC\Jan <jan.van.popta@hva.nl>
2017-09-17 22:50:23 +0200

    Propere .gitignore toegevoegd. en de scripts voor de basic powerup en de agilityPowerup

---
+++ GameJam1/Assets/Scripts/PowerUps/AgilityPowerup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+public class AgilityPowerup : BasicPowerup {
+
+    GameObject owner;
+
+    public AgilityPowerup(GameObject owner) {
+        this.owner = owner;
+    }
+
+    public float jumpModifier = 2f, moveSpeedModifier = 1.3f;
+
+    public override void Equip() {//allemaal super simpel nog want ik weet niet hoe de player class er precies uitziet.
+        //owner.jumpForce *= jumpModifier;
+        //owner.movementSpeed *= moveSpeedModifier;
+    }
+
+    public override void DeEquip() {
+        //owner.jumpForce = (owner.jumpForce / jumpModifier) * 1;
+        //owner.movementSpeed = (owner.movementSpeed / moveSpeedModifier) * 1;
+    }
+
+}
