95699e1750e8bcce9dd5653dd73ba2f9196f2d84
J.A. van Popta <jan.van.popta@hva.nl>
2017-09-19 23:27:07 +0200

    Uploaden van Base class via laptop aangezien desktop git problemen heeft met dientificatie, ik zal dan wel nog wat veranderingen aan movement opnieuw moeten doen maar dat lukt wel.

---
+++ GameJam1/Assets/Scripts/Base.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BaseCbaracter : MonoBehaviour {
+
+    public float movementSpeed = 2.0f;
+    protected float rotationSpeed;
+    public float jumpForce = 10f;
+
+
+    public BaseCbaracter() {
+        rotationSpeed = movementSpeed * 20;
+    }
