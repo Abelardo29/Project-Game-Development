f552ee16bd767f712ed407047d9287309136be26
LAPTOP-GM0HB9EQ\Jop <jop.koene@hva.nl>
2017-09-20 21:07:42 +0200

    Enemy Model

---
+++ GameJam1/Assets/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    // Use this for initialization
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        float translation = 5f;
+        float rotation = 25f;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+    }
+}
