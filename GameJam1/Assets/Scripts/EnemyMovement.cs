2b05c09c3ea7c72b0357d05840de10a2be6642d2
J.A. van Popta <jan.van.popta@hva.nl>
2017-09-21 03:03:18 +0200

    Commiting rudimentary vision system. IT SUCKS, BUT IT WORKS

--- GameJam1/Assets/Scripts/EnemyMovement.cs
+++ GameJam1/Assets/Scripts/EnemyMovement.cs
@@ -4,31 +4,32 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour {
 
     // Use this for initialization
-    public Rigidbody rb;
+    Rigidbody rb;
     Transform player;               // Reference to the player's position.
     PlayerHealth playerHealth;      // Reference to the player's health.
-    EnemyHealth enemyHealth;        // Reference to this enemy's health.
+    EnemyHealth me;        // Reference to this enemy's health.
     NavMeshAgent nav;               // Reference to the nav mesh agent.
+    [HideInInspector()]
+    public bool chasing;
 
     void Start() {
         // Set up the references.
         player = GameObject.FindGameObjectWithTag("Player").transform;
         playerHealth = player.GetComponent<PlayerHealth>();
-        enemyHealth = GetComponent<EnemyHealth>();
+        me = GetComponent<EnemyHealth>();
         nav = GetComponent<NavMeshAgent>();
+        chasing = false;
     }
 
 
     void Update() {
-        // If the enemy and the player have health left...
-        if (enemyHealth.health > 0 && playerHealth.health > 0) {
-            // ... set the destination of the nav mesh agent to the player.
-            nav.SetDestination(player.position);
-        }
-        // Otherwise...
-        else {
-            // ... disable the nav mesh agent.
-            nav.enabled = false;
+        if (chasing) {
+            // Als we nog hp hebben en de speler nog leeft
+            if (me.health > 0 && playerHealth.health > 0) {
+                nav.SetDestination(player.position);
+            } else {
+                nav.enabled = false;
+            }
         }
     }
 }
---
+++ GameJam1/Assets/Scripts/EnemyVision.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVision : MonoBehaviour {
+
+    public EnemyMovement movement;
+    //we geven gewoon de hele player aan de vision klas, want waarom niet?
+    //Het is fucking 2 uur 's nachts dus aantal fucks gaat het raam uit.
+    GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+    float range = 200;
+	
+	// Update is called once per frame
+	void Update () {
+        if (CanSeePlayer()) {
+            movement.chasing = true;
+        }		
+	}
+
+    public bool CanSeePlayer() {
+
+        RaycastHit hit;
+        //player.transform.position - transform.position;
+        Vector3 rayDirection = transform.forward; 
+
+
+
+            if (Physics.Raycast(transform.position, rayDirection, out hit)) { // If the player is very close behind the player and in view the enemy will detect the player
+                if (hit.transform.tag == "Player"){//korte notitie dat dit dus alleen recht voor zich kijkt atm.
+                    return true;
+                }
+            }
+
+            /*if ((Vector3.Angle(rayDirection, transform.forward)) & lt; fieldOfViewRange){ // Detect if player is within the field of view
+                if (Physics.Raycast(transform.position, rayDirection, hit)) {
+
+                    if (hit.transform.tag == "Player") {
+                        Debug.Log("Can see player");
+                        return true;
+                    }
+                    else {
+                        Debug.Log("Can not see player");
+                        return false;
+                    }
+                }
+            }*/
+        return false;
+        }   
+}
