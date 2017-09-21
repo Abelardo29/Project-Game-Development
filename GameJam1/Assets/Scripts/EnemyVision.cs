ef29432b4ad208ab2209acaff2c53551d88f856d
J.A. van Popta <jan.van.popta@hva.nl>
2017-09-21 11:50:01 +0200

    aanpassingen aan EnemyVision. + scene

--- GameJam1/Assets/Scripts/EnemyVision.cs
+++ GameJam1/Assets/Scripts/EnemyVision.cs
@@ -1,25 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class EnemyVision : MonoBehaviour {
 
     public EnemyMovement movement;
     //we geven gewoon de hele player aan de vision klas, want waarom niet?
     //Het is fucking 2 uur 's nachts dus aantal fucks gaat het raam uit.
     GameObject player;
-    [SerializeField]CapsuleCollider visionCone;//not really a cone but whatever
+    //[SerializeField]CapsuleCollider visionCone;//not really a cone but whatever
+    public LineRenderer visionLine = new LineRenderer();
+    public float range;
 
 
     void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
+        DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y, range), Color.red, 500);
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (CanSeePlayer()) {
             movement.chasing = true;
-        }		
+        }
+        
 	}
 
     public bool CanSeePlayer() {
@@ -47,5 +50,20 @@ public class EnemyVision : MonoBehaviour {
                 }
             }*/
         return false;
-        }   
+        }
+
+    void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.2f) {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = start;
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+        lr.startColor = color;
+        lr.endColor =  color;
+        lr.startWidth = 0.1f;
+        lr.endWidth = 0.1f;
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        GameObject.Destroy(myLine, duration);
+    }
 }
