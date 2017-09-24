ac2b59d287abc1a2cc22ae0e4daa9a628c7269b2
J.A. van Popta <jan.van.popta@hva.nl>
2017-09-25 00:01:25 +0200

    tussendoor commit met wat ik heb gedaan voor het geval dat. Niet alles, sommige code zou voorkomen dat unity runt en je wil geen code committen die voorkomt dat andere mensen kunnen testen

---
+++ GameJam2/Assets/Scripts/Weapon/BaseWeapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseWeapon : MonoBehaviour {//Gemaakt door Jan van Popta.
+    //Dit is de klasse die weaponswitches regelt.
+
+
+    public BridgeBuilder bridgeAttachment;
+    
+    [HideInInspector()]
+    List<BuildMechanic> attachments = new List<BuildMechanic>();
+
+
+    //Start () hier controleert welke attachments we aan het wapen hebben gegeven, zo houden we capaciteiten van de speler modulair.
+    void Start () {
+        if (bridgeAttachment != null)
+            attachments.Add(bridgeAttachment);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ GameJam2/Assets/Scripts/Weapon/BuildMechanic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public  abstract class BuildMechanic : MonoBehaviour {
+
+    public virtual void Update() {
+        if (Input.GetMouseButton(0)) {
+            CreateTerrain();        
+        }
+    }
+
+    protected abstract void CreateTerrain();
+}
