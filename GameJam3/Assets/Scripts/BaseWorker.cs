78fe84b9bde64e31956d2d9bd987a2d7d139ea43
JANPC\Jan <jan.van.popta@hva.nl>
2017-10-03 00:28:24 +0200

    Committing BaseWorker code, hij kan nu naar aangegeven gebieden lopen. Hij moet nog kunnen interacten met de wereld om hem heen, dan is ie klaar.

--- GameJam3/Assets/Scripts/BaseWorker.cs
+++ GameJam3/Assets/Scripts/BaseWorker.cs
@@ -6,10 +6,15 @@ public class BaseWorker : MonoBehaviour {
 
     public Vector2 baseCamp;
     public string training = "none";
+    public float movementSpeed;
 
     [HideInInspector()]
     public Vector2 playerOrders; //a position to be set by the mouse.
+    public Vector3 goal;
 
+    public BaseWorker (string trainingParam) {
+        training = trainingParam;
+    }
 	// Use this for initialization
 	void Start () {
         transform.position = new Vector3(baseCamp.x, baseCamp.y, 0);
@@ -17,10 +22,21 @@ public class BaseWorker : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetMouseButtonDown(0))
+            goal = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 	}
 
-    void moveTo(Vector2 goal) {
+    private void LateUpdate() {
+            MoveToLocation(goal);
+    }
+
+    void MoveToLocation(Vector3 goal) {
+        transform.position = Vector3.MoveTowards(transform.position, goal, movementSpeed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+        
+    }
+
+    void MoveToBuilding() {
 
     }
 }
