a4d7a21160428a7e2b9f2f0ebba60639fa399693
J.A. van Popta <jan.van.popta@hva.nl>
2017-10-02 21:13:24 +0200

    Commiting some small changes and the start of a worker class

---
+++ GameJam3/Assets/Scripts/BaseWorker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseWorker : MonoBehaviour {
+
+    public Vector2 baseCamp;
+    public string training = "none";
+
+    [HideInInspector()]
+    public Vector2 playerOrders; //a position to be set by the mouse.
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(baseCamp.x, baseCamp.y, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void moveTo(Vector2 goal) {
+
+    }
+}
