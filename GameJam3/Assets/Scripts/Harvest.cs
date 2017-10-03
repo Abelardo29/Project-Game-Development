6d4adf330ab012215e8d502809c7782aa7bd530b
GP70-2PE\Gebruiker <sven.van.rijn2@hva.nl>
2017-10-03 23:20:09 +0200

    Begin Harvest mechanic
    het daadwerkelijk harvesten en testen moet nog gebeuren maar er zijn geen werkers.

---
+++ GameJam3/Assets/Scripts/Harvest.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Harvest : MonoBehaviour {
+
+    private float nIntervalHarvest;
+    private float intervalHarvest = 1;
+    private float totalHarvests = 10;
+
+    private string resourceTypeCol;
+	
+    public void HarvestResource(float startTime)
+    {
+        intervalHarvest = startTime;
+        if (nIntervalHarvest + intervalHarvest <= Time.time)
+        {
+            nIntervalHarvest += intervalHarvest;
+            switch (resourceTypeCol)
+            {
+                case "Stone":
+                    resourceTypeCol = "Stone";
+                    //Add harvest code;
+                    break;
+
+                case "Wood":
+                    resourceTypeCol = "Wood";
+                    //Add harvest code;
+                    break;
+            }
+        }
+        if (nIntervalHarvest > startTime + (totalHarvests * intervalHarvest))
+        {
+            // switch state to travelHome;
+            
+        }
+    }
+
+    private void OnCollision(Collision collision)
+    {
+        if (collision.gameObject.tag == "Resource")
+        {
+            HarvestResource(Time.time);
+        }
+        switch (collision.gameObject.GetComponent<ResourceType>().resourceType)
+        {
+            case "Stone":
+                resourceTypeCol = "Stone";
+                break;
+
+            case "Wood":
+                resourceTypeCol = "Wood";
+                break;
+        }
+    }
+}
