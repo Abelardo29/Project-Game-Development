4eca789df8cb58d8b4adb39ca33bacd6bf103312
LAPTOP-GM0HB9EQ\Jop <jop.koene@hva.nl>
2017-10-04 13:26:09 +0200

    UI Interface

---
+++ GameJam3/Assets/GetResourceUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetResourceUI : MonoBehaviour {
+
+    GameObject player;
+    ResourceManager resources;
+    public Text resourceText;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        resources = player.GetComponent<ResourceManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        resourceText.text = "HP: " + resources.hp + "                 Food: " + resources.food + "                 Wood: " + resources.wood + "                 Stone: " + resources.stone;
+    }
+}
--- GameJam3/Assets/Scripts/ResourceManager.cs
+++ GameJam3/Assets/Scripts/ResourceManager.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class ResourceManager : MonoBehaviour {
 
+    public int hp;
     public int wood;
     public int stone;
-    public int iron;
+    public int food;
 
 	// Use this for initialization
 	void Start () {
-		
+        hp = 100;
+        wood = 0;
+        food = 0;
+        stone = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        wood++;
 	}
 }
